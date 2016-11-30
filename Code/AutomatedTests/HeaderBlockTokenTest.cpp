34c9b4bf14fe3923bb0697a41c892b273fdf8e6e
Chris Blume <ProgramMax@gmail.com>
2016-11-30 00:00:33 -0800

    Update licensing to be The maxGif Contributors
    Currently, the copyright is held by Chris Blume.
    This is correct for the time being. But it is not inviting
    to others who may want to contribute to the project.
    
    Instead, create an AUTHORS file where contributors can
    add themselves and change the copyright ownership to
    The maxGif Contributors.
    
    Issue #21

--- Code/AutomatedTests/HeaderBlockTokenTest.cpp
+++ Code/AutomatedTests/HeaderBlockTokenTest.cpp
@@ -1,4 +1,6 @@
-// Copyright 2016, Chris Blume. All rights reserved.
+// Copyright 2016, The maxGif Contributors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #include <max/Testing/TestSuite.hpp>
 #include <maxGif/Parsing/Tokens.hpp>
